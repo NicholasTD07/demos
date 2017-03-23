fece53f497c726f9e9d77619d2245957c3247acb
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-03-23 15:50:50 +1100

    Play with CALayer and shadow

--- CALayer and Shadow/CALayer and Shadow/ViewController.swift
+++ CALayer and Shadow/CALayer and Shadow/ViewController.swift
@@ -18,7 +18,17 @@ class ViewController: UIViewController {
     }
 
     private func setupView() {
-        
+        let redView = UIView(frame: CGRect(x: 100, y:100, width: 100, height: 100))
+        redView.backgroundColor = .red
+
+        view.addSubview(redView)
+
+        let layer = redView.layer
+
+        layer.shadowOpacity = 0.3 // shows the shadow
+        // layer.shadowOffset = CGSize(width:0, height: 3) // moves the shadowCGC
+        let path = CGPath(rect: CGRect(x: layer.bounds.origin.x - 20, y: layer.bounds.origin.y + layer.bounds.height, width: layer.bounds.width + 2 * 20, height: 10), transform: nil)
+        layer.shadowPath = path
     }
 
     override func didReceiveMemoryWarning() {
