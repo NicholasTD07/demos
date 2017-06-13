c4a16133c938845c600b7e9c8c72c2771bddaa84
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-06-13 15:35:19 +1000

    [PhotosMap] For all photos with a location, show it on the map

--- PhotosMap/PhotosMap/ViewController.swift
+++ PhotosMap/PhotosMap/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import Photos
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let assets = PHAsset.fetchAssets(with: .image, options: PHFetchOptions())
+
+        print(assets.count)
+
+        assets.enumerateObjects { (asset, _, _) in
+            guard let coordinate = asset.location?.coordinate else { return }
+
+            let placemark = MKPlacemark(coordinate: coordinate)
+
+            self.mapView.addAnnotation(placemark)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
